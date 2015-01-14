c42d7a1567ffa6387ce0f57d5194cc4ff6ea8f99
Vladimir Makaric <vladamakaric@gmail.com>
2015-01-14 19:16:15 +0100

    add index.html with canvas element and simple animation in main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+var theCanvas = document.getElementById("myCanvas");
+var time = 0;
+c = theCanvas.getContext("2d");
+c.fillStyle = "blue";
+
+var render = function(){
+	c.clearRect(0,0, theCanvas.width, theCanvas.height);
+	var x=time;
+	var y=0;
+
+	c.fillRect(x,y,100,200);
+	time+=1;
+}
+
+setInterval(render,20);
