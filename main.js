29144a4776e3b38539a94bbfa89fb75f4fef53d0
Vladimir Makaric <vladamakaric@gmail.com>
2015-01-15 22:57:52 +0100

    change setInterval to requestAnimationFrame

--- main.js
+++ main.js
@@ -3,13 +3,14 @@ var time = 0;
 c = theCanvas.getContext("2d");
 c.fillStyle = "blue";
 
-var render = function(){
+var render = function(timestamp){
 	c.clearRect(0,0, theCanvas.width, theCanvas.height);
 	var x=time;
 	var y=0;
 
 	c.fillRect(x,y,100,200);
 	time+=1;
+	window.requestAnimationFrame(render);
 }
 
-setInterval(render,20);
+window.requestAnimationFrame(render);
